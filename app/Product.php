d51458ac2cecdef8a7da0fece986b7b1ac92bcd1
Alejandro <a.alvarezmontes@gmail.com>
2018-04-12 12:10:17 -0300

    Vista Welcome Lista

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    // $product->category
+    public function category()
+    {
+    	return $this->belongsTo(category::class);
+    }
+
+    // $product->images
+    public function images()
+    {
+    	return $this->hasMany(ProductImage::class);
+    }
 }
