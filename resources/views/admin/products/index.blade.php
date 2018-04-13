80c3e4bb699f5c38dd53a1ce85e79fe4f437c010
Alejandro <a.alvarezmontes@gmail.com>
2018-04-13 10:25:16 -0300

    CRUD Listo

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -5,7 +5,7 @@
 @section('body-class', 'product-page')
 
 @section('content')
-<div class="header header-filter" style="background-image: url('https://images.unsplash.com/photo-1423655156442-ccc11daa4e99?crop=entropy&dpr=2&fit=crop&fm=jpg&h=750&ixjsv=2.1.0&ixlib=rb-0.3.5&q=50&w=1450');"></div>
+<div class="header header-filter" style="background-image: url('https://images.unsplash.com/photo-1441986300917-64674bd600d8?ixlib=rb-0.3.5&s=94abdde7c5c34726196939f58e6ff8f3&auto=format&fit=crop&w=1350&q=80');"></div>
 
         <div class="main main-raised">
             <div class="container">
@@ -34,15 +34,19 @@
                                         <td>{{ $product->category ? $product->category->name : 'General' }}</td>
                                         <td class="text-right">{{ $product->price }}</td>
                                         <td class="td-actions text-right">
-                                            <button type="button" rel="tooltip" title="Ver producto" class="btn btn-info btn-simple btn-xs">
-                                                <i class="fa fa-info"></i>
-                                            </button>
-                                            <button type="button" rel="tooltip" title="Editar producto" class="btn btn-success btn-simple btn-xs">
-                                                <i class="fa fa-edit"></i>
-                                            </button>
-                                            <button type="button" rel="tooltip" title="Eliminar" class="btn btn-danger btn-simple btn-xs">
-                                                <i class="fa fa-times"></i>
-                                            </button>
+                                            <form method="post" action="{{ url('/admin/products/'.$product->id) }}">
+                                                @csrf
+                                                @method("DELETE")
+                                                 <a href="#" rel="tooltip" title="Ver producto" class="btn btn-info btn-simple btn-xs">
+                                                    <i class="fa fa-info"></i>
+                                                </a>
+                                                <a href="{{ url('/admin/products/'.$product->id.'/edit') }}" rel="tooltip" title="Editar producto" class="btn btn-success btn-simple btn-xs">
+                                                    <i class="fa fa-edit"></i>
+                                                </a>
+                                                <button type="submit" rel="tooltip" title="Eliminar" class="btn btn-danger btn-simple btn-xs">
+                                                    <i class="fa fa-times"></i>
+                                                </button>
+                                            </form>
                                         </td>
                                     </tr>
                                     @endforeach
