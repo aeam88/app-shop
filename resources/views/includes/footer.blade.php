f0acd955a92c48d4fe38479cd99799c0e0593be9
Alejandro Aeam <aeam@MacBook-Air-de-Alejandro.local>
2018-04-14 22:15:15 -0300

    Perfiles y validacion listo

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,31 @@
+<footer class="footer">
+            <div class="container">
+                <nav class="pull-left">
+                    <ul>
+                        <li>
+                            <a href="http://www.creative-tim.com">
+                                ¿Necesitas ayuda con algún producto?
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://presentation.creative-tim.com">
+                               Sobre nosotros
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://blog.creative-tim.com">
+                               Blog
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://www.creative-tim.com/license">
+                                Terminos y condiciones
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <div class="copyright pull-right">
+                    &copy; 2018, appshop.cl - Todos los derechos reservados.
+                </div>
+            </div>
+</footer>
