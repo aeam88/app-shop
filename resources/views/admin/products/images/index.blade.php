c8e7c357dbebd1bd243bb09321949c41d717e0ab
Alejandro Alvarez <a.alvarezmontes@gmail.com>
2018-04-16 20:09:57 -0300

    Ver imagenes productos

---
+++ resources/views/admin/products/images/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('title', 'Imágenes de productos')
+
+@section('body-class', 'product-page')
+
+@section('content')
+<div class="header header-filter" style="background-image: url('https://images.unsplash.com/photo-1441986300917-64674bd600d8?ixlib=rb-0.3.5&s=94abdde7c5c34726196939f58e6ff8f3&auto=format&fit=crop&w=1350&q=80');"></div>
+
+        <div class="main main-raised">
+            <div class="container">
+                <div class="section text-center">
+                    <h2 class="title">Imágenes del producto "{{ $product->name }}"</h2>
+
+                    <form method="post" action="">
+                        @csrf
+                        <input type="file" name="photo" required>
+                        <button type="submit" class="btn btn-primary">Subir nueva imágen</button>
+                        <a href="{{ url('/admin/products') }}" class="btn btn-default">Volver al listado de productos</a>
+                    </form>
+                    <hr>
+                    <div class="row">
+                        @foreach ($images as $image)
+                        <div class="col-md-4">
+                            <div class="panel panel-default">
+                                <div class="panel-body">
+                                    <img src="{{ $image->image }}" alt="">
+                                    <button type="submit" class="btn btn-danger">Eliminar imágen</button>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        @include('includes.footer')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,10 @@ Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
 	Route::post('/products', 'ProductController@store');
 	Route::get('/products/{id}/edit', 'ProductController@edit');
 	Route::post('/products/{id}/edit', 'ProductController@update');
-	Route::delete('/products/{id}', 'ProductController@destroy'); 
+	Route::delete('/products/{id}', 'ProductController@destroy');
+
+
+	Route::get('/products/{id}/images', 'ImageController@index');
+	Route::post('/products/{id}/images', 'ImageController@store');
+	Route::delete('/products/{id}/images', 'ImageController@destroy'); 
 });
